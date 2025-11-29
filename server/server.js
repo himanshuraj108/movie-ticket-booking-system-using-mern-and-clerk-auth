5b8b3b8f4b140e3e78fb90c35f2ede220f5d4336
Himanshu <himanshuraj48512@gmail.com>
2025-11-30 00:24:06 +0530

    Inngest and Clerk connection with deployable vercel.json created

--- server/server.js
+++ server/server.js
@@ -2,19 +2,26 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import {serve} from 'inngest/express';
+import { inngest,functions } from "./inngest/index.js";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-await connectDB()
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
+
 
 app.get("/", (req, res) => {
   res.send("API SERVER IS RUNNING...");
 });
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
