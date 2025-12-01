d03d440ea153ac89477b0077ee9b77c53080648f
Himanshu <himanshuraj48512@gmail.com>
2025-12-02 01:46:20 +0530

    Show Controllers

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,9 +1,78 @@
+import axios from "axios";
+import Movie from "../models/Movie.js";
+import Show from "../models/Shows.js";
 
+export const getNowPlayingMovies = async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/movie/now_playing",
+      {
+        headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+      }
+    );
 
-export const getNowPlayingMovies = async(req,res)=>{
-    try {
-        
-    } catch (error) {
-        
+    const movies = data.results;
+    res.json({ success: true, movies: movies });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const addShow = async (req, res) => {
+  try {
+    const { movieId, showInput, showPrice } = req.body;
+    let movie = await Movie.findById(movieId);
+    if (!movie) {
+      const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+          headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` },
+        }),
+      ]);
+      const movieApiData = movieDetailsResponse.data;
+      const movieCreditsData = movieCreditsResponse.data;
+
+      const movieDetails = {
+        _id: movieId,
+        title: movieApiData.title,
+        overview: movieApiData.overview,
+        poster_path: movieApiData.poster_path,
+        backdrop_path: movieApiData.backdrop_path,
+        genres: movieApiData.genres,
+        casts: movieCreditsData.cast,
+        release_date: movieApiData.release_date,
+        original_language: movieApiData.original_language,
+        tagline: movieApiData.tagline || "",
+        runtime: movieApiData.runtime,
+      };
+
+      movie = await Movie.create(movieDetails);
     }
-}
+
+    const showToCreate = [];
+    showInput.forEach((show) => {
+      const showDate = show.date;
+      show.time.forEach((time) => {
+        const dateTimeString = `${showDate}T${time}`;
+        showToCreate.push({
+          movie: movieId,
+          showDateTime: new Date(dateTimeString),
+          showPrice,
+          occupiedSeats: {},
+        });
+      });
+    });
+
+    if (showToCreate.length > 0) {
+      await Show.insertMany(showToCreate);
+    }
+
+    res.json({ success: true, message: "Show Added Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
-import {serve} from 'inngest/express';
-import { inngest,functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoute.js";
 
 const app = express();
 
@@ -16,12 +17,11 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-
 app.get("/", (req, res) => {
   res.send("API SERVER IS RUNNING...");
 });
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
+app.use("/api/show", showRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
